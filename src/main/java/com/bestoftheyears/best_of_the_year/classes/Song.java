bcd2399ebf73e8d7629c57d7fb15266ba39c8411
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-15 17:21:25 +0200

    Implement SongController and songs template; add song retrieval logic

--- src/main/java/com/bestoftheyears/best_of_the_year/classes/Song.java
+++ src/main/java/com/bestoftheyears/best_of_the_year/classes/Song.java
@@ -1,22 +1,17 @@
 package com.bestoftheyears.best_of_the_year.classes;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Song {
 
     private int id;
     private String title;
-    public static List<Song> listOfSongs = new ArrayList<>();
 
     public Song (String title, int id){
         this.title=title;
         this.id=id;
         
     }
-
-
-
 
     public int getId() {
         return this.id;
--- src/main/java/com/bestoftheyears/best_of_the_year/controllers/SongController.java
+++ src/main/java/com/bestoftheyears/best_of_the_year/controllers/SongController.java
@@ -1,5 +1,32 @@
 package com.bestoftheyears.best_of_the_year.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bestoftheyears.best_of_the_year.classes.Song;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
 public class SongController {
+
+    private List<Song> getBestSongs() {
+        List<Song> songsList = new ArrayList<>();
+        songsList.add(new Song("Bohemian Rhapsody", 1));
+        songsList.add(new Song("Imagine", 2));
+        songsList.add(new Song("Smells Like Teen Spirit", 3));
+        return songsList;
+    }
+
+    @GetMapping("/songs")
+    public String getSongs(Model model) {
+        model.addAttribute("songs", getBestSongs());
+        return "songs";
+    }
 
 }
