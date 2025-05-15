e4fac2323dec7beca98b2dcce724469af7b36a9f
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-15 16:01:15 +0200

    first commit

---
+++ src/main/java/com/bestoftheyears/best_of_the_year/controllers/homeController.java
@@ -0,0 +1,21 @@
+package com.bestoftheyears.best_of_the_year.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class homeController {
+    
+    @GetMapping("/")
+    public String getMethodName(Model model, @RequestParam String name) {
+        model.addAttribute("name",name);
+        return "home";
+    }
+    
+
+}
