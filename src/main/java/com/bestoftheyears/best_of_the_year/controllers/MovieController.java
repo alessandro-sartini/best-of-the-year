01eb5b5c25456b32541bfe1f45c0d9fe1a0458c2
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-16 10:23:54 +0200

    Add movie retrieval by ID and single movie template

--- src/main/java/com/bestoftheyears/best_of_the_year/controllers/MovieController.java
+++ src/main/java/com/bestoftheyears/best_of_the_year/controllers/MovieController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.bestoftheyears.best_of_the_year.classes.Movie;
 
 @Controller
@@ -24,6 +26,28 @@ public class MovieController {
     public String getMovies(Model model) {
         model.addAttribute("movies", getBestMovies());
         return "movies";
+    }
+    @GetMapping("/movies/{id}")
+    public String getSingleMovies(Model model, @PathVariable int id) {
+        Movie foundMovie=null;
+        for (Movie movie : getBestMovies()) {
+            
+            if (movie.getId()==id) {
+                foundMovie=movie;
+                break;
+            }
+
+        }
+
+        if (foundMovie!=null) {
+            model.addAttribute("movie",foundMovie);
+        }else{
+            String notFound="nessun film trovato";
+            model.addAttribute("errorMessage",notFound);
+        }
+
+        // model.addAttribute("movies", getBestMovies());
+        return "singleMovie";
     }
 
 }
